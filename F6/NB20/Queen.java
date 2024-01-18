49b215e7428dda8cf177ed6827eaef216b34dac3
remove11 <90849964+remove11@users.noreply.github.com>
2024-01-18 21:12:19 +0100

    nb20

--- F6/NB20/Queen.java
+++ F6/NB20/Queen.java
@@ -1,42 +1,78 @@
 package F6.NB20;
 
 public class Queen {
-    int board [][] = new int[8][8];
-    public Queen() {
+    int board [][];
+    public Queen(int row, int col){
+        this.board = new int[row][col];
         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board.length; j++){
+            for(int j = 0; j < board[i].length; j++){
                 board[i][j] = 0;
             }
         }
     }
 
-    public boolean addQueen(){
-        return addQueen(0 ,0);
+    public boolean addQueen() {
+        return addQueen(0, "");
     }
 
-    private boolean addQueen(int row, int col){
-        if(legalCheck(row, col)){
-
+    private boolean addQueen(int col, String positions) {
+        if (col == board.length) {
+            System.out.println(positions);
+            return true;
         }
-        return false;
-    }
 
-    private boolean legalCheck(int row, int col){
-        for(int i = 0; i < board.length; i++){
-            if(board[row][i] == 0) return true;
-        }
-        for(int i = row; i < board.length; i++){
-            for(int j = col; j < board.length; j++){
-                if(board[i][j] == 0) return true;
+        for (int row = 0; row < board.length; row++) {
+            if (legalCheck(row, col)) {
+                board[row][col] = 1;
+                if (addQueen(col + 1, positions + "(" + row + ", " + col + "), ")) {
+                    return true;
+                }
+                board[row][col] = 0;
             }
         }
         return false;
     }
+
+    /*private boolean legalCheck(int row, int col){
+        for(int i = 0; i < col; i++){
+            if(board[row][i] == 1) return false;
+        }
+        for (int i = row; i >= 0; i--) {
+            for (int j = col; j >= 0; j--) {
+                if (row - i == col - j) {
+                    if (board[i][j] == 1) return false;
+                }
+            }
+        }
+        for (int i = row; i >= 0; i++) {
+            for (int j = col; j >= 0; j--) {
+                if (row - i == col - j) {
+                    if (board[i][j] == 1) return false;
+                }
+            }
+        }
+        return true;
+    }*/
+
+    private boolean legalCheck(int row, int col) {
+        for (int i = 0; i < col; i++) {
+            if (board[row][i] == 1) return false;
+        }
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 1) return false;
+        }
+        for (int i = row, j = col; i < board.length && j >= 0; i++, j--) {
+            if (board[i][j] == 1) return false;
+        }
+        return true;
+    }
+
+
     public String toString(){
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board.length; j++){
-                sb.append(board[i][j] + ", ");
+                sb.append(board[i][j] == 1 ? "Q " : "- ");
             }
             sb.append("\n");
         }
@@ -44,11 +80,13 @@ public class Queen {
     }
 
     public static void main(String[] args) {
-        Queen queen = new Queen();
-        System.out.println(queen.toString());
-        System.out.println(queen.legalCheck(1,1));
-
-
+        Queen queen = new Queen(8,8);
+        if (queen.addQueen()) {
+            System.out.println("En lösning:");
+            System.out.println(queen);
+        } else {
+            System.out.println("Ingen lösning hittades.");
+        }
     }
 
 }
